5e0aaa76bdc5aff4bfbcd057db012e6ce1061283
H.Motta <henrique2008790@gmail.com>
2025-05-05 16:32:45 -0300

    comentarios

--- aula6/atv1.py
+++ aula6/atv1.py
@@ -1,3 +1,3 @@
-for numero in range(100):
-    if (numero %2 == 0):
+for numero in range(100): #um laço de retição onde ele repetre 100 vezes os códigos abaixo
+    if (numero %2 == 0): #se o número for divisível por 2 ele mostra o número
         print(numero)
--- aula6/atv3.py
+++ aula6/atv3.py
@@ -1,17 +1,19 @@
-numpess = int(input("Quantas pessoas serão analisadas:"))
-media = 0
-soma = 0
+numpess = int(input("Quantas pessoas serão analisadas:"))#pede ao usuário quantas pessoas serão analisadas
+media = 0 #declara a variável média
+soma = 0 #declara a variável soma
+ 
+ # cria um laço de repetição em que ele se repetirá de acordo com o número de pessoas que o usuário indicou que serão analisadas
 for numerodetemps in range(numpess):
-    temp = float(input(f"Qual a temperatura do paciente {numerodetemps + 1 }: "))
-    soma = soma + temp
-    media = soma/numpess
-    if temp <= 37.2:
+    temp = float(input(f"Qual a temperatura do paciente {numerodetemps + 1 }: ")) #qual a temperatura do paciente 
+    soma = soma + temp #soma as temperaturas dos pacientes analisados
+    media = soma/numpess # faz a média das temperaturas analisadas
+    if temp <= 37.2: #se a temperatura for menor que 37,2 graus define como normal
         print(f"A temperatura do paciente {numerodetemps + 1 } está normal")
-    elif (37.2 < temp <= 38 ):
+    elif (37.2 < temp <= 38 ): #se a temperatura for maior que 37,2 graus e menor que 38 graus define como estado febril
         print(f"A temperatura do paciente {numerodetemps + 1 } está em estado febril")
-    elif (38 < temp <= 39 ):
+    elif (38 < temp <= 39 ): #se a temperatura for maior que 38 graus e menor que 39 graus define como estado febril
         print(f"A temperatura do paciente {numerodetemps + 1 } está com febre")
-    elif (temp < 39 ):
+    elif (temp < 39 ): #se a temperatura for maior que 39 graus  define como febre alta
         print(f"A temperatura do paciente {numerodetemps + 1 } está com febre alta")
 
 
